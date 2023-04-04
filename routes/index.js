8a2380f6458095f8221ef278a56d1a5727759980
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-04-04 21:07:36 +0100

    add features

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', {
+    const features = [
+        {
+            title: 'Great Location',
+            icon: 'fas fa-thumbtack fa-3x',
+            color: 'grey'
+        },
+        {
+            title: 'Free Meals',
+            icon: 'fas fa-utensils fa-3x',
+            color: 'grey'
+        },
+        {
+            title: 'Fitness Room',
+            icon: 'fas fa-heartbeat fa-3x',
+            color: 'grey'
+        }
+    ];
+
+    res.render('index', {
         title: 'Murray Hotel | Welcome',
         lead: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quia commodi beatae sequi iusto itaque',
-        paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem rem ipsam optio dolorum fuga ducimus dolores. Earum voluptatem minus, porro sunt ullam quos enim excepturi nemo ab rerum totam non vitae illum sint repellat, tempore beatae perspiciatis cum. Assumenda, nobis.'
+        paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem rem ipsam optio dolorum fuga ducimus dolores. Earum voluptatem minus, porro sunt ullam quos enim excepturi nemo ab rerum totam non vitae illum sint repellat, tempore beatae perspiciatis cum. Assumenda, nobis.',
+        features: features
     });
 });
 
