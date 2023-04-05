31fe73bcf7cb907012d68e81a97a4e15a155eb04
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-04-05 20:59:20 +0100

    add tests to config 4

--- test/test_index.js
+++ test/test_index.js
@@ -1,42 +1,74 @@
-var assert = require('assert');
-var fs = require('fs');
-var path = require('path');
-const index = fs.readFileSync('views/index.ejs', 'utf8');
-var ejs = require('ejs');
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+const cheerio = require('cheerio');
 
-describe("index.ejs", () => {
-    it("should contain the correct title", () => {
-      const html = ejs.render(indexTemplate, { title });
-      const $ = cheerio.load(html);
-      expect($("title").text()).to.equal(title);
-    });
-  
-    it("should display the correct lead", () => {
-      const html = ejs.render(indexTemplate, { lead });
-      const $ = cheerio.load(html);
-      expect($(".lead").text()).to.equal(lead);
-    });
-  
-  it('should contain the correct paragraph', function() {
-    var rendered = ejs.render(index, { paragraph: 'Learn about the history of our hotel' });
-    assert(rendered.includes('Learn about the history of our hotel'));
+const indexTemplate = fs.readFileSync('views/index.ejs', 'utf8');
+
+describe('index.ejs', () => {
+  const data = {
+    title: 'My Hotel',
+    lead: 'Welcome to our luxurious hotel',
+    paragraph: 'Learn about the history of our hotel',
+    footer: 'Copyright © My Hotel',
+    features: [
+      {
+        title: 'Free Breakfast',
+        description: 'Enjoy a complimentary breakfast',
+        icon: 'fa-utensils',
+        color: 'green'
+      },
+      {
+        title: 'Fitness Center',
+        description: 'Stay in shape with our fitness center',
+        icon: 'fa-dumbbell',
+        color: 'blue'
+      },
+      {
+        title: 'Pool',
+        description: 'Take a dip in our pool',
+        icon: 'fa-swimming-pool',
+        color: 'yellow'
+      },
+      {
+        title: 'Room Service',
+        description: 'Order food to your room',
+        icon: 'fa-concierge-bell',
+        color: 'red'
+      }
+    ]
+  };
+
+  it('should contain the correct title', () => {
+    const html = ejs.render(indexTemplate, data);
+    const $ = cheerio.load(html);
+    assert.strictEqual($('title').text(), data.title);
   });
-  
-  it('should contain the correct footer', function() {
-    var rendered = ejs.render(index, { footer: 'Copyright © My Hotel' });
-    assert(rendered.includes('Copyright © My Hotel'));
+
+  it('should display the correct lead', () => {
+    const html = ejs.render(indexTemplate, data);
+    const $ = cheerio.load(html);
+    assert.strictEqual($('.lead').text(), data.lead);
   });
-  
-  it('should display the correct number of features', function() {
-    var features = [
-      { title: 'Free Breakfast', description: 'Enjoy a complimentary breakfast', icon: 'fa-utensils', color: 'green' },
-      { title: 'Fitness Center', description: 'Stay in shape with our fitness center', icon: 'fa-dumbbell', color: 'blue' },
-      { title: 'Pool', description: 'Take a dip in our pool', icon: 'fa-swimming-pool', color: 'yellow' },
-      { title: 'Room Service', description: 'Order food to your room', icon: 'fa-concierge-bell', color: 'red' }
-    ];
-    var rendered = ejs.render(index, { features: features });
-    var numFeatures = features.length;
-    var numBoxes = (rendered.match(/<div class="box/g) || []).length;
-    assert.equal(numFeatures, numBoxes);
+
+  it('should contain the correct paragraph', () => {
+    const html = ejs.render(indexTemplate, data);
+    const $ = cheerio.load(html);
+    assert.strictEqual($('#home-info p').text(), data.paragraph);
+  });
+
+  it('should contain the correct footer', () => {
+    const html = ejs.render(indexTemplate, data);
+    const $ = cheerio.load(html);
+    assert.strictEqual($('#main-footer').text().trim(), data.footer);
+  });
+
+  it('should display the correct number of features', () => {
+    const html = ejs.render(indexTemplate, data);
+    const $ = cheerio.load(html);
+    const numFeatures = data.features.length;
+    const numBoxes = $('.box').length;
+    assert.strictEqual(numBoxes, numFeatures);
   });
 });
