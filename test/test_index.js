49198521146339929a65bb5dabf33b9db2ebd1d2
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-04-05 20:49:10 +0100

    add tests to config

--- test/test_index.js
+++ test/test_index.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
-var index = fs.readFileSync(path.resolve(__dirname, '../index.ejs'), 'utf8');
+const index = fs.readFileSync('views/index.ejs', 'utf8');
 var ejs = require('ejs');
 
 describe('index.ejs', function() {
