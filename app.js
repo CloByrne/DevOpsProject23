6ea4ca847a47029d34679b08040d9dbd44218d10
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-04-05 19:53:32 +0100

    revert to hello world

--- app.js
+++ app.js
@@ -21,8 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/math', mathRouter);
-app.use('/about', aboutRouter);
-app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
