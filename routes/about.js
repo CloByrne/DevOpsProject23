752eb045bc06f3725a674940bc93873eb7916240
Clodagh Byrne <x22146229@student.ncirl.ie>
2023-04-05 18:53:23 +0100

    update route files to .ejs

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'About Us' });
+  res.render('about', { 
+    title: 'About',
+    hotelInfo: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima rerum neque ipsam qui aliquam consequuntur eveniet mollitia suscipit laborum similique ab vero a excepturi eaque exercitationem magni dolores, in animi?',
+    hotelDescription: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem rem ipsam optioLorem ipsum dolor sit amet consectetur, adipisicing elit. Officia molestias dolor quidem error eius expedita modi, corporis nisi fuga eos!',
+    testimonialOne:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum fugiat deserunt animi numquam! Deserunt, quas voluptate saepe sequi velit deleniti perspiciatis reprehenderit, amet ipsam totam est aliquam laudantium doloribus placeat culpa aliquid autem, maiores laboriosam at et numquam doloremque mollitia!',
+    testimonialTwo:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum fugiat deserunt animi numquam! Deserunt, quas voluptate saepe sequi velit deleniti perspiciatis reprehenderit, amet ipsam totam est aliquam laudantium doloribus placeat culpa aliquid autem, maiores laboriosam at et numquam doloremque mollitia!',    
+    footer: 'Hotel BT; 2023, All Rights Reserved'
+  });
 });
 
 module.exports = router;
+
+
+router.get('/', function(req, res, next) {
+  res.render('contact', { 
+    
+  });
+});
--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('contact', { title: 'Contact Us' });
+  res.render('contact', { 
+    title: 'Contact Us',
+    lead: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quia commodi beatae sequi iusto itaque',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem rem ipsam optio dolorum fuga ducimus dolores. Earum voluptatem minus, porro sunt ullam quos enim excepturi nemo ab rerum totam non vitae illum sint repellat, tempore beatae perspiciatis cum. Assumenda, nobis.',
+    footer: 'Hotel BT; 2023, All Rights Reserved'
+  });
 });
 
 module.exports = router;
+
